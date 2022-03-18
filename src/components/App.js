354f553f594706f98b64fbb31d7cf4b16bba8840
Jacob <your_email@real_email.edu>
2022-03-18 09:21:52 -0500

    palette works and uses css animations

---
+++ src/components/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+
+/**
+ * The main application uses these four components
+ */
+import Header from './Header';
+import Palette from './Palette';
+import Grid from './Grid';
+import ActionPanel from './ActionPanel';
+
+/**
+ * We need access to COLORS and buildCellList for our initial
+ * state objects
+ */
+import {
+  COLORS,
+  buildCellList
+} from '../utils';
+
+/**
+ * The App component represents our entire application. It contains all of the
+ * top level components, and is responsible for helping its children communicate 
+ * with each other via state objects, and state functions.
+ */
+const App = () => {
+  /**
+   * Using useState you need to create:
+   * 
+   * - activeColor, setActiveColor initialized to COLORS[0]
+   * - cellList, setCellList initialized to buildCellList()
+   */
+  const [activeColor, setActiveColor] = useState(COLORS[0])
+  const [cellList, setCellList] = useState(buildCellList())
+
+  return <div className="app">
+    {/* Header needs no props */}
+    <Header />
+    {/* Palette needs to be passed activeColor and setActiveColor */}
+    <Palette 
+      activeColor={activeColor}
+      setActiveColor={setActiveColor}/>
+    {/* Grid needs to be passed activeColor, cellList, and setCellList */}
+    <Grid />
+    {/* ActionPanel needs to be passed activeColor, cellList, and setCellList */}
+    <ActionPanel />
+  </div>
+}
+
+export default App;
